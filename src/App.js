3bde43d101a7bd38006d63b90ac44079c69f6e2a
Aravindan <aravindanravi33@gmail.com>
2021-02-07 21:47:39 +0530

    add: added the cypress setup successfully

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<div className="hello">Hello World</div>
+				<div className="hello">Hello world!</div>
 			</div>
 		);
 	}
