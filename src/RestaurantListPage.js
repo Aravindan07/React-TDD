fa16aa85f9ee904d8da25d93ac031b4608a26ab1
Aravindan <aravindanravi33@gmail.com>
2021-02-10 21:09:59 +0530

    add: added the first e2e test

--- src/RestaurantListPage.js
+++ src/RestaurantListPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewRestaurantForm from "./NewRestaurantForm";
+import RestaurantList from "./RestaurantList";
 
 export default class RestaurantListPage extends Component {
 	state = { restaurantNames: [] };
@@ -10,10 +11,12 @@ export default class RestaurantListPage extends Component {
 		}));
 	};
 	render() {
+		const { restaurantNames } = this.state;
 		return (
 			<div>
 				<button data-test="addRestaurantButton">Add Restaurant</button>
 				<NewRestaurantForm onSave={this.handleAddRestaurant} />
+				<RestaurantList restaurantNames={restaurantNames} />
 			</div>
 		);
 	}
